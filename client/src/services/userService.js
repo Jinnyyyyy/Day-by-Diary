49080e4ce2bf9a36898cfb7af93837944a4704b4
지니코 <jinico@Jinny-MacBook-Pro.local>
2021-12-09 22:27:36 -0500

    Deploy to Heroku

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,5 +1,5 @@
 import tokenService from './tokenService'
-const BASE_URL = 'http://localhost:3000/api/users'
+const BASE_URL = 'https://daybydiary.herokuapp.com/'
 
 function signup(user){
     return fetch(`${BASE_URL}/signup`, {
